af2174758304d16af1e51edddf9492119734c05f
Thore Suenert <mail@thoresuenert.de>
2020-10-19 12:24:58 +0200

    Test yarn workspaces

--- resources/views/welcome.inertia.vue
+++ resources/views/welcome.inertia.vue
@@ -2,5 +2,17 @@
     <div>
     <h1> Welcome to Teams package </h1>
     <p>I got loaded from package folder</p>
+    <p>{{ now }} </p>
     </div>
 </template>
+
+<script>
+import dayjs from 'dayjs';
+export default {
+    computed: {
+  now: function () {
+    return dayjs().format('{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
+  }
+}
+ };
+</script>
